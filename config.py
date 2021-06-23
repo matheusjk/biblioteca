a79a6287d87aebdd47179aa23180bf23dceaaecc
matheusjk <matheusrodriguesh1@gmail.com>
2021-06-23 13:40:44 -0300

    configuraçoes do sistema

---
+++ config.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://teste:123mudar@localhost:3306/atividades"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:""@localhost:3306/atividades"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "projetoBibli"
+
+MAIL_SERVER = ""
+MAIL_PORT = 587
+MAIL_USERNAME = ""
+MAIL_PASSWORD = ""
+MAIL_USE_TLS =  True
+MAIL_USE_SSL = False
